e1982c60eb6ba6db6166cd826cdeba1e07922070
Abbie <aitong0113@gmail.com>
2026-04-09 13:02:57 +0800

    Updates

--- src/utils/getImage.js
+++
@@ -1,9 +0,0 @@
-export function getImage(path) {
-  // 如果是外部網址 → 直接回傳
-  if (path.startsWith("http")) {
-    return path;
-  }
-
-  // 如果是本地圖片 → 自動加 base
-  return `${import.meta.env.BASE_URL}${path}`;
-}
